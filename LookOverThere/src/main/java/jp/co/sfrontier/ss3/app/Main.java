58fcec011d0d658c24e2db17c40ac577b4c28360
maeda-n12345 <smtrknnk.1626@gmail.com>
2025-09-19 11:13:30 +0900

    ロジェクト設定ファイルとMainクラスを追加

--- LookOverThere/src/main/java/jp/co/sfrontier/ss3/app/Main.java
+++ LookOverThere/src/main/java/jp/co/sfrontier/ss3/app/Main.java
@@ -4,6 +4,10 @@ package jp.co.sfrontier.ss3.app;
  * あっちむいてほいゲームのメインクラス
  */
 public class Main {
+	/**
+	 * あっち向いてほいを実行するためのメインメソッド
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		GameEngine game = new GameEngine();
 		game.play();
